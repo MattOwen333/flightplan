869b98e427541753533c4a332923c02185552050
suddenx <mattowen333@gmail.com>
2020-08-14 05:49:15 -0400

    final changes for the night

--- src/components/register.js
+++ src/components/register.js
@@ -7,32 +7,18 @@ import Modal from "react-modal";
 const Register = ({ toggleModal }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [location, setLocation] = useState("");
+
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-  const handleEmailChnage = (event) => {
-    setEmail(event.target.value);
-  };
-  const handleLocationChange = (event) => {
-    setLocation(event.target.value);
-  };
   async function handleSubmit(event) {
     event.preventDefault();
     const token = await register({
       username,
       password,
-      name,
-      email,
-      location,
     });
     localStorage.setItem("token", token);
     toggleModal(false);
@@ -53,24 +39,7 @@ const Register = ({ toggleModal }) => {
           value={password}
           onChange={handlePasswordChange}
         />
-        <input
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={handleEmailChnage}
-        />
-        <input
-          type="text"
-          placeholder="location"
-          value={location}
-          onChange={handleLocationChange}
-        />
+
         <button>Register</button>
       </form>
     </div>
