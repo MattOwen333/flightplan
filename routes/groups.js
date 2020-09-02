c4c84d90270fed4864deb3e238ab068d6a10918f
suddenx <mattowen333@gmail.com>
2020-09-02 06:41:59 -0400

    added joinGroup routing

--- routes/groups.js
+++ routes/groups.js
@@ -2,7 +2,12 @@ const express = require("express");
 const groupsRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { requireUser } = require("./utils");
-const { getAllGroups, createGroup, getGroupById } = require("../db");
+const {
+  getAllGroups,
+  createGroup,
+  getGroupById,
+  addUserToGroup,
+} = require("../db");
 
 groupsRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -11,14 +16,19 @@ groupsRouter.use((req, res, next) => {
 });
 
 groupsRouter.get("/", async (req, res) => {
-  const groups = await getAllGroups();
+  try {
+    const groups = await getAllGroups();
 
-  res.send({
-    groups,
-  });
+    res.send({
+      groups,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-groupsRouter.get("/", async (req, res, next) => {
+// GET /api/groups/mine
+groupsRouter.get("/mine", requireUser, async (req, res, next) => {
   const groups = await getUserGroupWithComments(req.user.id);
 
   res.send({ groups });
@@ -34,4 +44,58 @@ groupsRouter.post("/", requireUser, async (req, res, next) => {
   }
 });
 
-module.exports = groupsRouter;
+// need a route to JOIN a group
+groupsRouter.post("/:id/join", requireUser, async (req, res, next) => {
+  const creatorId = req.user.id;
+  try {
+    const joinGroup = await addUserToGroup(creatorId);
+    // read the user id off of req.user.id
+    // in here you will use a DB function called addUserToGroup(id)
+    // return the result of the user signing up
+    res.send({ joinGroup });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// groupsRouter.delete(':id', requireUser, async (req, res, next) =>
+//     const id = req.params.id;
+//     // you'd want to use deleteGroupById(id);
+//     // when a user hits /api/groups/7 with a DELETE request...
+// })
+
+routinesRouter.post(
+  "/:routineId/activities",
+  requireUser,
+  async (req, res, next) => {
+    const { activityId, count, duration } = req.body;
+    const { routineId } = req.params;
+
+    // await getRoutineById and then use next?
+
+    const routine = await addActivityToRoutine({
+      routineId,
+      activityId,
+      count,
+      duration,
+    });
+
+    res.send({
+      routine,
+    });
+  }
+);
+
+module.exports = groupsRouter; // from /index.js to /routes/index.js we get /api/groups/
+
+/**
+ * You'll do this in index.js:
+ *
+ * const groupRoutes = require('./groups');
+ * server.use('/groups', groupRoutes);
+ *
+ * GET /api/groups/3 => should give me a group with id 3
+ * GET /api/users => should give me all users
+ * POST /api/comments => should make a new comment
+ * PATCH /api/users/7 => should update user 7's info
+ */
--- routes/users.js
+++ routes/users.js
@@ -70,7 +70,7 @@ usersRouter.post("/login", async (req, res, next) => {
 });
 
 usersRouter.post("/register", async (req, res, next) => {
-  const { username, password, name, email, location } = req.body;
+  const { username, password } = req.body;
 
   try {
     const _user = await getUserByUsername(username);
